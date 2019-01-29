647654be64beb9a568bed71b6237b6aa03598a23
Steven Huang <steven.charles.huang@gmail.com>
2019-01-29 14:58:27 -0500

    formatted Greetr lib prototype and alias

---
+++ scripts/app.js
@@ -0,0 +1,3 @@
+let g = G$('John', 'Doe');
+
+console.log({g});
---
+++ scripts/libs/Greetr.js
@@ -0,0 +1,20 @@
+(function (global, $) {
+  let Greetr = function(firstName, lastName, language) {
+    return new Greetr.init(firstName, lastName, language);
+  };
+
+  Greetr.prototype = {};
+
+  Greetr.init = function(firstName, lastName, language) {
+    let self = this;
+    self.firstName = firstName || '';
+    self.lastName = lastName || '';
+    self.language = language || 'en';
+  }
+
+  Greetr.init.prototype = Greetr.prototype;
+
+  global.Greetr = global.G$ = Greetr;
+
+}(window, jQuery));
+
